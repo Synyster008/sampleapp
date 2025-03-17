8cfd8dc99e0efe75a31b7769a1161498964c1d9b
Farhan Javed <71452948+Synyster008@users.noreply.github.com>
2025-03-17 04:38:08 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sampleapp00",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "setuptools>=61",
+        "setuptools_scm>=8",
+    ],
+)
